154ad51c8324b82938c86de7257c492013f30ea4
Mauricio Fuentes <mauriciofb@gmail.com>
2024-08-28 19:37:13 -0400

    primeras 2 sesiones

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+console.log('Estamos usando JS')
+
+/**
+ * Palabras reservadas
+ * 
+ * let: asignar un valor a una variable
+ * cons: asigna valor a una constante
+ * var: asigna valor a una variable (forma antigua)
+ * console: herramientas de consola
+ * function: definir funciones
+ */
+
+/**
+ * Función sin input, y con efecto secundario
+ * alertadora es el nombre
+ * () : acá irá el input
+ * {} : cuerpo de la función
+ */
+function alertadora () {
+  /** cuerpo de la función */
+  alert('Estoy ejecutando la función alertadora')
+}
